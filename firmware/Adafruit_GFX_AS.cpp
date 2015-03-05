04194827fae020caca251d69ce665f34c8d3617f
Menan Vadivel <menan89@gmail.com>
2015-03-04 20:20:21 -0500

    updated pgm

--- firmware/Adafruit_GFX_AS.cpp
+++ firmware/Adafruit_GFX_AS.cpp
@@ -53,9 +53,12 @@ POSSIBILITY OF SUCH DAMAGE.
   #include "Font7s.h"
 #endif
 
-// just two to start with
-#define pgm_read_byte(addr) (*(const unsigned char *)(addr))
-#define pgm_read_word(addr) (*(const unsigned short *)(addr))
+
+#if defined(SPARK)
+  // just two to start with
+  #define pgm_read_byte(addr) (*(const unsigned char *)(addr))
+  #define pgm_read_word(addr) (*(const unsigned short *)(addr))
+#endif
 
 Adafruit_GFX_AS::Adafruit_GFX_AS(int16_t w, int16_t h):
   WIDTH(w), HEIGHT(h)
